0ca3176b2819d4a69d96a67906acb94e04e4fd17
daneryl <daneryl@gmail.com>
2016-09-19 12:04:31 +0200

    conversions migration to run on uwazi entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,6 +16,7 @@ if [ "$1" = "database" ]; then
 fi
 
 if [ "$1" = "uwazi" ]; then
+    cd couchdb && cd migrations && node run_migration.js && cd .. && cd ..
     cd couchdb && ./restore_views.sh $DATABASE_NAME && cd ..
     exec node server "$@"
 fi
